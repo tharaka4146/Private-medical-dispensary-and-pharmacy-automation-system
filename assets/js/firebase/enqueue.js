96aeadb545ae25fd38f06fb69ed9143f7de43305
tharaka4146 <tharaka4146@gmail.com>
2021-07-15 22:47:39 +0530

    firebase date time update

--- assets/js/firebase/enqueue.js
+++ assets/js/firebase/enqueue.js
@@ -6,7 +6,25 @@ const database = firebase.database();
 enqueue.addEventListener('click', (e) => {
     e.preventDefault();
 
-    database.ref('/users/' + nic.value).set({
-        nic: nic.value
+    const currentDate = Date.now();
+
+    /*
+        const d = new Date(currentDate)
+    
+        const year = d.getFullYear()
+        const month = d.getMonth()
+        const date = d.getDate()
+    
+        const hours = d.getHours()
+        const minutes = d.getMinutes()
+    
+        const dateTime = d.toLocaleString()
+        console.log(dateTime)
+    
+        */
+    database.ref('/queue/' + nic.value).set({
+        nic: nic.value,
+        dateTime: currentDate
     });
+
 });
